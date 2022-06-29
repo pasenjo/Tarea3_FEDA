36cc026fae4d573747e5208b0f7537898a9595d3
pasenjo <48869100+pasenjo@users.noreply.github.com>
2022-06-28 22:07:23 -0400

    Update hashADT.h

--- hashADT.h
+++ hashADT.h
@@ -6,6 +6,9 @@
 #include <vector>
 #include <algorithm>
 
+// Nos basamos en el código creado en ayudantía, pero modificando levemente, agregando la pseudoclase
+// datos, que consiste en un par, de primera componente un string y segunda componente un 
+// vector. Esta es nuestra estructura para guardar los datos.
 
 static const int hash_primos[] = {53, 97, 193, 389, 769, 1543, 3079, 6151, 12289, 24593, 49157};
 
@@ -22,7 +25,6 @@ class hashADT{
 		virtual bool insert(datos) = 0;
 		virtual bool borrar(std::string) = 0;
 		virtual int buscar(std::string) = 0;
-		// virtual void print() = 0;
 	private:
 		virtual void resize() = 0;
 		virtual int hash_foo(std::string) = 0;
